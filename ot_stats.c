a8005c52fcc3ffa444caf05bfb8b0a4ccd7ff4aa
Tom Sullivan <tom@msbit.com.au>
2017-03-19 21:43:22 +1100

    On announce, log peer_id as string

--- ot_stats.c
+++ ot_stats.c
@@ -646,7 +646,7 @@ void stats_issue_event( ot_status_event event, PROTO_FLAG proto, uintptr_t event
       if( event_data) {
         struct ot_workstruct *ws = (struct ot_workstruct *)event_data;
         char timestring[64];
-        char hash_hex[42], peerid_hex[42], ip_readable[64];
+        char hash_hex[42], ip_readable[64];
         struct tm time_now;
         time_t ttt;
 
@@ -655,18 +655,13 @@ void stats_issue_event( ot_status_event event, PROTO_FLAG proto, uintptr_t event
         strftime( timestring, sizeof( timestring ), "%FT%T%z", &time_now );
 
         to_hex( hash_hex, *ws->hash );
-        if( ws->peer_id )
-          to_hex( peerid_hex, (uint8_t*)ws->peer_id );
-        else {
-          *peerid_hex=0;
-        }
 
 #ifdef WANT_V6
         ip_readable[ fmt_ip6c( ip_readable, (char*)&ws->peer ) ] = 0;
 #else
         ip_readable[ fmt_ip4( ip_readable, (char*)&ws->peer ) ] = 0;
 #endif
-        syslog( LOG_INFO, "time=%s event=announce info_hash=%s peer_id=%s ip=%s", timestring, hash_hex, peerid_hex, ip_readable );
+        syslog( LOG_INFO, "time=%s event=announce info_hash=%s peer_id=%s ip=%s", timestring, hash_hex, ws->peer_id, ip_readable );
       }
 #endif
       if( proto == FLAG_TCP ) ot_overall_tcp_successfulannounces++; else ot_overall_udp_successfulannounces++;
